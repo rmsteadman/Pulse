efbea7bed4af0a81552133c04a2b26e2531e5011
Mark Steadman <r.m.steadman@gmail.com>
2016-12-29 13:32:48 -0800

    [setup] Updates main query file

--- server/queries/index.js
+++ server/queries/index.js
@@ -1,6 +1,13 @@
-// import all models
-// ex const userModel = require(path to shit)
-
 module.exports = {
-
+  accountQuery: require('./account'),
+  beaconQuery: require('./beacon'),
+  categoryQuery: require('./category'),
+  chatroomQuery: require('./chatroom'),
+  historyQuery: require('./history'),
+  preferencesQuery: require('./preferences'),
+  ratingQuery: require('./rating'),
+  requestQuery: require('./request'),
+  rsvpQuery: require('./rsvp'),
+  statusQuery: require('./status'),
+  userQuery: require('./user')
 };
