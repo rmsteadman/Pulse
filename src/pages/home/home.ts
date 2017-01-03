51487fdaa2d4008d479b3e789c9d58c85129153f
Mark Steadman <r.m.steadman@gmail.com>
2017-01-02 16:57:47 -0800

    [setup] Inject AuthService to be used in view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,9 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
 import { CreateBeaconPage } from '../create-beacon/create-beacon'
+import {AuthService} from '../../services/auth/auth.service';
 declare var google;
- 
+
 @Component({
   selector: 'home-page',
   templateUrl: 'home.html'
@@ -13,57 +14,60 @@ export class HomePage {
   createBeacon: any = CreateBeaconPage;
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-  
-  constructor(public navCtrl: NavController) {}
- 
+
+  constructor(
+    public navCtrl: NavController,
+    public auth: AuthService
+  ) {}
+
   ionViewDidLoad(){
     this.loadMap();
   }
- 
+
   loadMap(){
- 
+
     Geolocation.getCurrentPosition()
       .then((position) => {
- 
+
         let center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  
+
         let mapOptions = {
           center: center,
           zoom: 15,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-  
+
         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-  
+
       }, (err) => {
         console.log(err);
       });
- 
+
   }
 
   addBeacon(){
- 
+
     let beacon = new google.maps.Marker({
       map: this.map,
       animation: google.maps.Animation.DROP,
       position: this.map.getCenter()
     });
     localStorage.setItem('currentLocation', beacon.position)
-    let content = "TEST";          
-  
+    let content = "TEST";
+
     this.addInfoWindow(beacon, content);
- 
+
   }
 
   addInfoWindow(beacon, content){
- 
+
     let infoWindow = new google.maps.InfoWindow({
       content: content
     });
-  
+
     google.maps.event.addListener(beacon, 'click', () => {
       infoWindow.open(this.map, beacon);
     });
-  
+
     }
 }
