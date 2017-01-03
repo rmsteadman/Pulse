48c712a6eff36a7013378150068423f504a4f4b9
Justin Biele <justin.biele@gmail.com>
2017-01-02 22:35:53 -0800

    [add] Add profile with preference list

--- server/models/preferences.js
+++ server/models/preferences.js
@@ -2,7 +2,14 @@ const Sequelize = require('sequelize');
 
 module.exports = (Models) => {
 
-  const Preferences = Models.define('Preferences', {});
+  const Preferences = Models.define('Preferences', {
+    preferences: {
+      type: Sequelize.TEXT
+    },
+    photo: {
+      type: Sequelize.TEXT
+    }
+  });
 
   return Preferences;
 };
