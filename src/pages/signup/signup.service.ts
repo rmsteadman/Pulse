2c951d9b9a082227b8a613317ad2f28d5dbf28e4
Mark Steadman <r.m.steadman@gmail.com>
2017-01-03 16:04:17 -0800

    [setup] Provides some logic clean up

--- src/pages/signup/signup.service.ts
+++ src/pages/signup/signup.service.ts
@@ -5,20 +5,23 @@ import 'rxjs/add/operator/map';
 
 
 @Injectable()
-export class SignupService {
-    constructor(public http: Http) {}
+export class SignUpService {
+  constructor(public http: Http) {}
 
-    signupPost(form): Observable<any> {
-        let user = {
-            firstName: form.firstName,
-            lastName: form.lastName,
-            email: form.email,
-            phoneNumber: form.phoneNumber,
-            password: form.password
-        }
-        return this.http.post('http://localhost:8080/api/users/signup', user)  
-            .map(data => {
-                console.log( data.json() )
-            })
+  signupPost(user): Observable<any> {
+    let User = {
+      accountId: user.accountId || 1,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      password: user.password,
+      authCred: user.user_id,
+      authToken: user.idToken
+    }
+    return this.http.post('http://localhost:8080/api/users/signup', User)
+      .map(data => {
+        console.log( data.json() )
+      })
     };
 }
