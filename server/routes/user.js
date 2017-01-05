932fe245c230910ce27248520c8e33e590d88134
Mark Steadman <r.m.steadman@gmail.com>
2017-01-04 22:54:51 -0800

    [progress] Adds functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,7 @@ let userRouter = require('express').Router();
 
 // routes
 // userRouter.get('/allusers', userController.getAllUsers)
+userRouter.post('/login', userController.login);
 userRouter.post('/signup', userController.signUp);
 userRouter.put('/prefs', userController.savePrefs);
 
