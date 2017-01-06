f16a6742e2bcc84bf3e4a442b248f25469467d99
Mark Steadman <r.m.steadman@gmail.com>
2017-01-06 13:33:59 -0800

    [progress] Fixes definition for consistency and adds chatroom creation func

--- server/queries/chatroom.js
+++ server/queries/chatroom.js
@@ -1,11 +1,16 @@
 const Chatroom = require('../models').Chatroom;
 
-const chatroomQuery = {};
+const chatroomQueries = {};
 
 // create a chatroom
+userQueries.create = (data) => {
+  return Chatroom.create({
+    name: data.name,
+    chat: '[]'
+  });
+};
 // delete a chatroom
 // find a chatroom by beacon
 // clear chat
-//
 
-module.exports = chatroomQuery;
+module.exports = chatroomQueries;
