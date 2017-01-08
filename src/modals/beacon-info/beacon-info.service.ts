a650aef26c39607cc6c75b7a190f446f4acdbd94
Mark Steadman <r.m.steadman@gmail.com>
2017-01-07 18:36:34 -0800

    [progress] Adds get messages function

--- src/modals/beacon-info/beacon-info.service.ts
+++ src/modals/beacon-info/beacon-info.service.ts
@@ -10,18 +10,24 @@ export class BeaconInfoService {
 
     constructor(public http: Http) {}
 
-    getMessages(chatroomId) /*: Observable<any>*/ {
-        console.log('in the get: ', chatroomId)
-        // return this.http.get('http://localhost:8080/api/chatrooms/getmessages')
-        //     .map(data => {
-        //         console.log(data)
-        //     })
-    }
+    getMessages(chatroomId): Observable<any> {
+            console.log('in the get: ', chatroomId)
+            // let config = {
+            //   params: {
+            //     chatroomId: chatroomId
+            //   }
+            // }
+            return this.http.get(`http://localhost:8080/api/chatrooms/getmessages/${chatroomId}`)
+                .map(data => {
+                    console.log("This is data from getMessages", data)
+                    return data.json();
+                })
+        }
 
 
     sendMessage(message): Observable<any> {
         console.log("THIS IS THE MESSAGE: ", message);
-        
+
         return this.http.put('http://localhost:8080/api/chatrooms/addmessage', message)
             .map(data => {
                 console.log('things are ok')
