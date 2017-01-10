4ab5e093996178a1a5c1d6ad4292e3f1cb0d3df0
Mark Steadman <r.m.steadman@gmail.com>
2017-01-09 17:08:39 -0800

    [progress] Adds function that fetchces RSVPs by beacon id

--- server/controllers/rsvp.js
+++ server/controllers/rsvp.js
@@ -2,22 +2,34 @@ const rsvpQuery = require('../queries').rsvpQuery;
 const userQuery = require('../queries').userQuery;
 const rsvpController = {};
 
-// logic
-
+// make an rsvp
 rsvpController.create = (req, res) => {
-    console.log("GOT INTO CONTROLLOER", req.body)
-    let rsvp = {
-        UserId: null,
-        BeaconId: req.body.id,
-        details: req.body.details
-    };
-    userQuery.findUser(req.body.token)
+  console.log('GOT INTO CONTROLLOER', req.body);
+  let rsvp = {
+    UserId: null,
+    BeaconId: req.body.id,
+    details: req.body.details
+  };
+  userQuery.findUser(req.body.token)
     .then(results => {
-        rsvp.UserId = results.id
-        return rsvpQuery.create(rsvp);
-    })
-    
-}
+      rsvp.UserId = results.id;
+      return rsvpQuery.create(rsvp);
+    });
+};
 
+// get rsvps by beacon
+rsvpController.getByBeacon = (req, res) => {
+  // console.log('this is req', req.params)
+  let id = req.params.id;
+  rsvpQuery.findRSVPs(id)
+    .then(results => {
+      // console.log('rsvp results', results)
+      (results) ? res.send(results) : res.send({});
+    })
+    .catch(err => {
+      console.log('Error in rsvp controller, (getByBeacon)', err);
+      return err;
+    });
+};
 
 module.exports = rsvpController;
