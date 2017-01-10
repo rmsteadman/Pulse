e74f94e79db1bdd3649631edb00342768c2139da
Mark Steadman <r.m.steadman@gmail.com>
2017-01-09 17:10:12 -0800

    [progress] Adds rsvp query function

--- server/queries/rsvp.js
+++ server/queries/rsvp.js
@@ -1,20 +1,31 @@
 const RSVP = require('../models').RSVP;
-
-const rsvpQuery = {};
+const User = require('../models').User;
+const rsvpQueries = {};
 
 // create an RSVP
-rsvpQuery.create = (rsvpData) => {
-    return RSVP.create(rsvpData)
+rsvpQueries.create = (rsvpData) => {
+  return RSVP.create(rsvpData)
     .then(data => {
-    return data;
+      return data;
     })
     .catch(err => {
-    console.log('Error in RSVP Query file');
+      console.log('Error in RSVP Query file');
       return err;
     });
-}
+};
 // delete an RSVP
 // update an RSVP details
 // find RSVPs by beacon
+rsvpQueries.findRSVPs = (data) => {
+  // console.log('data in findRSVP:', data)
+  return RSVP.findAll({
+    include: [
+      { model: User, required: true}
+    ],
+    where: {
+      BeaconId: data
+    }
+  });
+};
 
-module.exports = rsvpQuery;
+module.exports = rsvpQueries;
